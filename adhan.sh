7702334e34f309ce9c2d512279781a5a6447b559
Auvray Sébastien <auvraysebastien75@gmail.com>
2024-03-13 07:45:30 +0100

    Add log for script adhan

--- adhan.sh
+++ adhan.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Lancement de l'adhan
+echo "Lancement de l'adhan a $(date '+%Y-%m-%d %H:%M:%S')"
 python3 /app/home_assistant.py --action "turn_on"
+
 sleep 60
+
+# Reset bouton pour adhan
+echo "Réinitialisation du bouton homekit a $(date '+%Y-%m-%d %H:%M:%S')"
 python3 /app/home_assistant.py --action "turn_off"
